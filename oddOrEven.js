a6807978eacf404e9ba6523f00e37f03a7bac20f
Krithika Jagadeesan <krithikajagadeesan@marco-macbook.localdomain>
2019-02-15 11:21:36 -0800

    updated oddOrEven.js

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,14 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
-}
+function oddOrEven(num){
+  if (num%2===0){
+    return "Even"
+  } else if (num%2!==0) {
+    return "Odd"
+  }
 
+}
+console.log(oddOrEven(num))
 oddOrEven(4)
 oddOrEven(11)
 oddOrEven(75)
